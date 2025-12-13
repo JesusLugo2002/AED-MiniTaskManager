e0fc0021b658fa75e418adfaf259c53db4b2f4b2
JesusLugo2002 <jesusdlugo02@gmail.com>
2025-12-13 19:10:23 +0000

    chore: add error handling on async functions

--- src/repositories/repositorioTareasRemoto.ts
+++ src/repositories/repositorioTareasRemoto.ts
@@ -5,43 +5,70 @@ export class repositorioTareasRemoto implements ITareaRepository {
   private API_URL = "http://localhost:8080/api/tasks";
 
   async findAll(): Promise<Tarea[]> {
-    const response = await fetch(this.API_URL);
-    const data = await response.json();
+    let data = [];
+    try {
+      const response = await fetch(this.API_URL);
+      data = await response.json();
+    } catch (err) {
+      console.error("Something happend while trying fetch all tasks", err);
+    }
     return data;
   }
 
   async findById(id: IdTarea): Promise<Tarea | undefined> {
-    const response = await fetch(`${this.API_URL}/${id}`)
-    const data = await response.json();
+    let data = undefined;
+    try {
+      const response = await fetch(`${this.API_URL}/${id}`);
+      data = await response.json();
+    } catch (err) {
+      console.error(
+        `Something happend while trying fetch the task with ID ${id}`,
+        err
+      );
+    }
     return data;
   }
-  
+
   async create(title: string, description?: string): Promise<Tarea> {
-    const options = {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ title: title, description: description }),
-    };
-    const response = await fetch(this.API_URL, options);
-    const data = await response.json();
+    let data;
+    try {
+      const options = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ title: title, description: description }),
+      };
+      const response = await fetch(this.API_URL, options);
+      data = await response.json();
+    } catch (err) {
+      console.error(`Something happend while trying create a new task`, err);
+    }
     return data;
   }
 
   async update(task: Tarea): Promise<Tarea | undefined> {
-    const options = {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(task),
-    };
-    const response = await fetch(`${this.API_URL}/${task.id}`, options);
-    const data = await response.json();
+    let data = undefined;
+    const taskId = task.id;
+    try {
+      const options = {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(task),
+      };
+      const response = await fetch(`${this.API_URL}/${taskId}`, options);
+      data = await response.json();
+    } catch (err) {
+      console.error(
+        `Something happend while trying to update the task with ID ${taskId}`,
+        err
+      );
+    }
     return data;
   }
 
   async deleteById(id: number): Promise<boolean> {
-    const options = { method: "DELETE" }
+    const options = { method: "DELETE" };
     const response = await fetch(`${this.API_URL}/${id}`, options);
     return response.ok;
   }
